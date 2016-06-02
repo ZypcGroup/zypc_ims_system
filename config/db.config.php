2742d6eb3f6e7c22d9c77b7397e0df5c44ece623
hexiu <hexiu@users.noreply.github.com>
2016-06-03 00:31:02 +0800

    重要更改，信息泄露

--- config/db.config.php
+++ config/db.config.php
@@ -5,7 +5,7 @@
 define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
 define('DB_NAME', 'zypc_db');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'ps2s0nnet');
+define('DB_USER', '×××××');
+define('DB_PASSWORD', '×××××××');
 
 ?>
